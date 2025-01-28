981c195fb463ee29504d364b1d749a0881bff017
Julie Bissø <U2507981@rts-365.dk>
2025-01-28 10:11:36 +0100

    quotes added

---
+++ script.js
@@ -0,0 +1,15 @@
+let quotes = [
+    "Accept what people offer. Drink their milkshakes. Take their love.",
+    "I think high self-esteem is overrated. A little low self-esteem is actually quite good…Maybe you're not the best, so you should work a little harder.",
+    "An opinion should be the result of thought, not a substitute for it.",
+    "Don't make use of another's mouth unless it has been lent to you.",
+    "Eternity is a mere moment, just long enough for a joke.",
+    "Silence is a text easy to misread.",
+    "Life is tough, and if you have the ability to laugh at it you have the ability to enjoy it.",
+    "Gratitude can sometimes be as annoying as whininess.",
+    "Now and then an innocent man is sent to the legislature.",
+    "To be ignorant of one's ignorance is the malady of the ignorant"
+];
+
+let quoteListElm = document.querySelector(".siteFooter__quote blockquote");
+let i = 0;
